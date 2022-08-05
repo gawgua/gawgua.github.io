7d31f35cbdce193676c631b6321e432de3dad99c
gawgua <taisaodeoduoc@gmail.com>
2022-08-05 13:22:41 +0700

    change to Lanyard WebSocket

--- scripts/discordProfile.js
+++ scripts/discordProfile.js
@@ -1,8 +1,51 @@
-setInterval(reloadProfile, 2000);
+const socket = new WebSocket("wss://api.lanyard.rest/socket");
+var HEARTBEAT = 30000;
 
-function reloadProfile(){
-    let date = new Date();
-    let timestamp = date.getTime();
-    let image = document.getElementById('profileImage');  
-    image.src = 'https://lanyard.cnrad.dev/api/518362974411292691' + '?t=' + timestamp; 
+socket.addEventListener("open", () => {
+    socket.send(
+        JSON.stringify({
+            op: 2,
+            d: {
+                "subscribe_to_id": "518362974411292691"
+            },
+        }),
+    );
+
+    socket.addEventListener("message", ({ data }) => {
+        const message = JSON.parse(data);
+        if (message.op == 1){
+            HEARTBEAT = message.d.heartbeat_interval;
+        }else{
+            console.log(JSON.parse(data));
+        }
+    });
+
+    setInterval(() => {
+        socket.send(
+            JSON.stringify({
+                op: 3,
+            }),
+        );
+        console.log("3");
+    }, HEARTBEAT);
+});
+
+async function checkDiscordStatus(status){
+    if (status === "online"){
+        //change img src
+    }
+    if (status === "idle"){
+        //change img src
+    }
+    if (status === "dnd"){
+        //change img src
+    }
+    if (status === "offline"){
+        //change img src
+    }
+}
+
+async function profileImage(userId, avatarId){
+    //set image to url 
+    const url = `https://cdn.discordapp.com/avatars/${userId}/${avatarId}.png`;
 }
